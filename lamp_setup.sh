3fedcbebad10481eb7807faea3ba44a8d3495814
Abu Shahriar Ratul <ratulcse10@gmail.com>
2016-01-15 05:25:12 +0600

    lamp added

--- lamp_setup.sh
+++ lamp_setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Welcome to Easy Ubuntu Dev Setup 1.0"
-echo "Maintained By SUST CSE Developer Network (SCDN)"
+echo -e "Maintained By SUST CSE Developer Network (SCDN)\n"
+echo "Steap:1 [System Update]"
+echo "Update Starts....."
+sudo apt-get update
+echo -e "System Update Completed Successfully\n"
+echo "Step:2 [Install Apache2]"
+sudo apt-get install apache2
+echo -e "Apache2 Installation Completed Successfully\n"
+echo "Thanks  !!!"
