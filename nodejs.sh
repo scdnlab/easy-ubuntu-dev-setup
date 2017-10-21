9a401fc2a8fc9b4ed0e44bdc303c80d8b0c7845e
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2017-10-21 13:04:44 +0600

    yes flag added

--- nodejs.sh
+++ nodejs.sh
@@ -9,12 +9,12 @@ sudo apt-get update
 echo -e "System Update Completed Successfully\n"
 
 echo "Step:2 [Install Node.js]"
-sudo apt-get install nodejs
+sudo apt-get install -y nodejs
 sudo ln -s "$(which nodejs)" /usr/bin/node
 echo -e "Node.js Installation Completed Successfully\n"
 
 echo "Step:3 [Install Node.js Package Manager - npm]"
-sudo apt-get install npm
+sudo apt-get install -y npm
 echo -e "npm Installation Completed Successfully\n"
 
 echo "Thanks  !!!"
