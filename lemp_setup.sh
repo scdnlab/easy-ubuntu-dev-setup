89bcef37263ff7f0ab602f7df72e21e7412e7389
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2018-02-07 03:51:11 +0600

    major update

--- lemp_setup.sh
+++ lemp_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Welcome to Easy Ubuntu Dev Setup 2.0"
+echo "Welcome to Easy LEMP Stack Setup 3.0"
 echo -e "Maintained By SUST CSE Developer Network (SCDN)\n"
 
 echo "Steap:1 [System Update]"
@@ -18,7 +18,7 @@ echo -e "MySQL Installation Completed Successfully\n"
 
 echo "Step:4 [Install PHP7.2]"
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y 
 sudo apt-get -y update
 sudo apt-get install -y php7.2
 udo apt-cache search php7.2
@@ -26,24 +26,65 @@ sudo apt-get install -y php-redis php7.2-cli php7.2-fpm php7.2-mysql php7.2-curl
 
 echo -e "PHP 7.2 Installation Completed Successfully\n"
 
+sudo systemctl start php7.2-fpm
+sudo systemctl enable php7.2-fpm
+
 echo "Step:5 [Install PHPmyadmin]"
 sudo apt-get install phpmyadmin
 echo -e "PHPmyadmin Installation Completed Successfully\n"
 
 sudo systemctl restart nginx
 
+echo "Remove the default symlink in sites-enabled directory"
+sudo rm /etc/nginx/sites-enabled/default
+cat > /etc/nginx/sites-enabled/default <<EOF
+
+server {
+  listen 80;
+  listen [::]:80;
+  server_name localhost;
+  root /usr/share/nginx/html/;
+  index index.php index.html index.htm index.nginx-debian.html;
+
+  location / {
+    try_files $uri $uri/ =404;
+  }
+
+  error_page 404 /404.html;
+  error_page 500 502 503 504 /50x.html;
+
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+
+  location ~ \.php$ {
+    fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include fastcgi_params;
+    include snippets/fastcgi-php.conf;
+  }
+
+  location ~ /\.ht {
+    deny all;
+  }
+}
+
+EOF
+
+sudo nginx -t
+sudo systemctl reload nginx
+
 echo "Step:6 [Install curl]"
-sudo apt-get install curl
+sudo apt-get install -y curl
 echo -e "curl Installation Completed Successfully\n"
 
 echo "Step:7 [Install Git]"
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 echo -e "Git Installation Completed Successfully\n"
 
 echo "Step:8 [Install mcrypt and enable rewrite]"
-sudo php7.2-mcrypt mcrypt
 sudo service php7.2-fpm restart
 sudo systemctl restart nginx
 echo -e "mcrypt Installation and enable rewrite Completed Successfully\n" 
